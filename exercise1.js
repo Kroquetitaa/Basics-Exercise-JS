cc0d86091631519841e3cd4d46e0b1424838a064
Kroquetitaa <Kroquetita@gmail.com>
2022-07-26 00:46:06 +0200

    Exercise example

--- exercise1.js
+++ exercise1.js
@@ -14,3 +14,28 @@ const avengersLength = calcListLength(avengers);
 const mutantsLength = calcListLength(mutants);
 const defendersLength = calcListLength(defenders);
 console.log( avengersLength, mutantsLength, defendersLength );
+
+const comparator = ( a, b, c ) => {
+    if( a > b ){
+        if ( a === c) return 'Empate entre a y c';
+        if( a > c ){
+            return 'a gana';
+        } else {
+            return 'c gana';
+        }
+    } else {
+        if ( a == b ) return 'Empate entre a y b';
+        if( b > c ) {
+            return 'b gana';
+        } else{
+            return 'c gana';
+        }
+    }
+}
+
+console.log(comparator(avengersLength,mutantsLength, defendersLength));
+
+let media = ( a, b, c) => {
+    return (a + b + c) / 3;
+}
+console.log(media(avengersLength,mutantsLength, defendersLength));
